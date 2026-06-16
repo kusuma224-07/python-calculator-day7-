1eccf279e4417e6952c9acec752792475f8aae4b
kusuma <viku170219@gmail.com>
2026-06-16 11:35:08 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,6 @@
+a=10
+b=50
+print("the value of",a,"+",50,"is:", a + b)
+print("the value of",a,"-",50,"is:", a - b)
+print("the value of",a,"/",50,"is:", a / b)
+print("the value of",a,"*",50,"is:", a * b) 
